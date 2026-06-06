925a56520958831e5d3e53c3c421311c84127e38
panicbit <panicbit.dev@gmail.com>
2026-06-06 03:14:53 +0200

    add file logging with rotation

--- src/logging.rs
+++ src/logging.rs
@@ -1,16 +1,55 @@
-use color_eyre::eyre::{Context, Result};
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+use std::sync::Mutex;
+
+use color_eyre::eyre::{Context, ContextCompat, Result};
+use file_rotate::compression::Compression;
+use file_rotate::suffix::AppendCount;
+use file_rotate::{ContentLimit, FileRotate};
 use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{FmtSubscriber, fmt};
 
 pub fn init() -> Result<()> {
-    let subscriber = FmtSubscriber::builder()
+    let terminal_subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .without_time()
         .with_target(false)
         .finish();
+    let file_subscriber = fmt::layer()
+        .without_time()
+        .with_target(false)
+        .with_writer(Mutex::new(log_rotate_writer()?));
 
-    tracing::subscriber::set_global_default(subscriber)
+    tracing::subscriber::set_global_default(terminal_subscriber.with(file_subscriber))
         .context("failed to set default tracing subscriber")?;
 
     Ok(())
+}
+
+fn log_dir() -> Result<PathBuf> {
+    let project_dirs = directories::ProjectDirs::from("com.github", "panicbit", "muffle")
+        .context("failed to get project dirs")?;
+    let state_dir = project_dirs
+        .state_dir()
+        .context("failed to get state dir")?;
+
+    Ok(state_dir.to_path_buf())
+}
+
+fn log_rotate_writer() -> Result<impl Write> {
+    let log_dir = log_dir().context("failed to get log dir")?;
+
+    fs::create_dir_all(&log_dir).context("failed to create log dir")?;
+
+    let log_path = log_dir.join("log.txt");
+
+    Ok(FileRotate::new(
+        log_path,
+        AppendCount::new(3),
+        ContentLimit::Lines(3),
+        Compression::None,
+        None,
+    ))
 }
